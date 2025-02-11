9cf1d6baa3d5bc47f75b064ad418014bedae7f58
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-02-11 09:45:02 -0500

    Initial Commit

---
+++ Lab1_Brendan_Dasilva/Lab1_Brendan_DasilvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Brendan_DasilvaApp.swift
+//  Lab1_Brendan_Dasilva
+//
+//  Created by Brendan Dasilva on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Brendan_DasilvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1_Brendan_DasilvaTests/Lab1_Brendan_DasilvaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Brendan_DasilvaTests.swift
+//  Lab1_Brendan_DasilvaTests
+//
+//  Created by Brendan Dasilva on 2025-02-11.
+//
+
+import Testing
+@testable import Lab1_Brendan_Dasilva
+
+struct Lab1_Brendan_DasilvaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
